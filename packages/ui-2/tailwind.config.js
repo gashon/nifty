091ea3aa50443c1fabe17a1fb87a79fd0a98f29c
GashonHussein <gashon96@gmail.com>
2023-03-23 15:36:03 -0700

    chore: storybook tailwind config

--- packages/ui-2/tailwind.config.js
+++ packages/ui-2/tailwind.config.js
@@ -2,7 +2,7 @@ const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 const { join } = require('path');
 
 module.exports = {
-  presets: [require('../../tailwind.config')],
+  presets: [require('../../apps/client/tailwind.config.js')],
   content: [
     join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
