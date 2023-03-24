528da7ff10d29507348607491a67b09d651facca
GashonHussein <gashon96@gmail.com>
2023-03-23 18:18:52 -0700

    feat: index export ui

---
+++ packages/ui/src/atoms/index.ts
@@ -0,0 +1,7 @@
+export * from './brand';
+export * from './button';
+export * from './dropdown-menu';
+export * from './input';
+export * from './nav-link';
+export * from './progress-bar';
+export * from './star-button'; 
---
+++ packages/ui/src/molecules/index.ts
@@ -0,0 +1,6 @@
+export * from './greeting'
+export * from "./modal"
+export * from "./module-card"
+export * from "./notebook-item"
+export * from "./sidebar-link"
+export * from "./user-card"
