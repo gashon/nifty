6b661a988d37c7dc6db2535dd02397c07112dee6
GashonHussein <gashon96@gmail.com>
2023-03-24 20:12:07 -0700

    fix: add github oauth email check

--- apps/api/src/lib/passport.ts
+++ apps/api/src/lib/passport.ts
@@ -28,11 +28,16 @@ const googleStrategy = new GoogleStrategy(
 const githubStrategy = new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID!,
   clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-  callbackURL: `${process.env.API_BASE_URL}/ajax/auth/login/github/callback`
+  callbackURL: `${process.env.API_BASE_URL}/ajax/auth/login/github/callback`,
+  scope: ['user:email', 'read:user'],
 },
   async (accessToken, refreshToken, profile, cb) => {
+    console.log("GOT PROFILE", profile);
+    const email = profile.emails?.find((e: any) => e.primary?.value) || profile.emails?.[0]?.value;
+    if (!email) return cb(new Error('No email found'));
+    
     const user: IUser = await User.findOneAndUpdate(
-      { email: profile._json.email },
+      { email },
       {
         avatar: profile._json.avatar_url,
         last_login: Date.now(),
