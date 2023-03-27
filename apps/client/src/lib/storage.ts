9ab1fdafd03814969f0c9390b123dfe1168bfdf7
GashonHussein <gashon96@gmail.com>
2023-03-26 17:02:29 -0700

    feat: support meta-data in storage

--- apps/client/src/lib/storage.ts
+++ apps/client/src/lib/storage.ts
@@ -1,11 +1,28 @@
-const storagePrefix = 'l_s';
+const storagePrefix = 'G_H';
 
-const storage = {
+type StorageDocument<T> = {
+  data: T;
+  created_at: Date;
+  updated_at: Date;
+  expires_at?: Date;
+} | null;
+
+type Storage = {
+  get<T>(key: string): T | null;
+  set<T>(key: string, value: T, expires_at?: Date): void;
+  remove(key: string): void;
+  exists(key: string): boolean;
+  getMeta(key: string): Omit<StorageDocument<any>, 'data'> | null;
+  isMounted(): boolean;
+  isAvailable(): boolean;
+};
+
+const storage: Storage = {
   get<T>(key: string): T | null {
     if (!this.isMounted()) throw new Error('Storage is not available');
 
     const item = localStorage.getItem(`${storagePrefix}_${key}`);
-    const doc = item ? JSON.parse(item) : null;
+    const doc: StorageDocument<T> = item ? JSON.parse(item) : null;
 
     if (doc && doc.expires_at && new Date(doc.expires_at) < new Date()) {
       this.remove(key);
@@ -17,17 +34,33 @@ const storage = {
   set<T>(key: string, value: T, expires_at?: Date): void {
     if (!this.isMounted()) throw new Error('Storage is not available');
 
-    localStorage.setItem(`${storagePrefix}_${key}`, JSON.stringify({ data: value, expires_at }));
+    const prev = this.getMeta(key);
+
+    const doc: StorageDocument<T> = {
+      data: value,
+      created_at: prev ? prev.created_at : new Date(),
+      updated_at: new Date(),
+      expires_at,
+    };
+
+    localStorage.setItem(`${storagePrefix}_${key}`, JSON.stringify(doc));
   },
   remove(key: string): void {
-    if (!storage.isMounted()) throw new Error('Storage is not available');
+    if (!this.isMounted()) throw new Error('Storage is not available');
 
     localStorage.removeItem(`${storagePrefix}_${key}`);
   },
   exists(key: string): boolean {
-    if (!storage.isMounted()) throw new Error('Storage is not available');
+    if (!this.isMounted()) throw new Error('Storage is not available');
 
     return !!localStorage.getItem(`${storagePrefix}_${key}`);
+  },
+  getMeta(key: string): Omit<StorageDocument<any>, 'data'> | null {
+    const item = localStorage.getItem(`${storagePrefix}_${key}`);
+    const doc: StorageDocument<any> = item ? JSON.parse(item) : null;
+
+    delete doc.data;
+    return doc
   },
   isMounted(): boolean {
     return typeof window !== 'undefined';
