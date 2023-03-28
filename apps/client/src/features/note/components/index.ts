32c2ccd9a4f6dfe98cfb5d10b66fbf9675fca86e
GashonHussein <gashon96@gmail.com>
2023-03-28 04:24:15 -0700

    feat: react-quill

--- apps/client/src/features/note/components/index.ts
+++ apps/client/src/features/note/components/index.ts
@@ -1,1 +1,2 @@
 export * from "./note-creation"
+export * from "./editor"
--- apps/client/src/pages/notes/[id].tsx
+++ apps/client/src/pages/notes/[id].tsx
@@ -1,19 +1,26 @@
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
-import { lazy } from 'react';
+import dynamic from 'next/dynamic';
+import { useState, useEffect } from 'react';
 
 import ThemeLayout from '@/layouts/theme';
-import DashboardLayout from '@/layouts/dashboard';
 import { AuthProtection, useAuth } from '@/features/auth';
-import { NotebookList } from '@/features/module';
+// import { DocumentEditor } from '@/features/note/';
+const DocumentEditor = dynamic(() => import('@/features/note/components/editor'), { ssr: false });
 
 import { LoadingPage } from '@nifty/ui/pages/loading';
 
-export default function Module() {
+export default function Document() {
   const router = useRouter();
   const { isOffline } = useAuth();
-
   const { documentId } = router.query;
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted || typeof window === 'undefined') return null;
 
   if (isOffline) {
     // todo handle offline mode here
@@ -28,6 +35,11 @@ export default function Module() {
         <ThemeLayout>
           <main className="flex flex-col order-1 pt-9">
             <h3 className="pb-6 text-3xl text-primary dark:text-zinc-400 ">Editor</h3>
+            conte
+            <div className="h-screen">
+              <DocumentEditor />
+            </div>
+            done
           </main>
         </ThemeLayout>
       </AuthProtection>
