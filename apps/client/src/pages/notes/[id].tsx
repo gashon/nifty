11a55f50e2626096f06cf827670fafa29410ce8d
GashonHussein <gashon96@gmail.com>
2023-03-30 19:02:14 -0700

    feat: ssr editor

--- apps/client/src/pages/notes/[id].tsx
+++ apps/client/src/pages/notes/[id].tsx
@@ -1,18 +1,17 @@
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
-import dynamic from 'next/dynamic';
 import { useState, useEffect } from 'react';
 import { BsArrowBarLeft } from 'react-icons/bs';
 
-import { AuthProtection, useAuth } from '@/features/auth';
+import { AuthProtection, AuthProvider, getUser } from '@/features/auth';
 import { LoadingPage } from '@nifty/ui/pages/loading';
 import { DocumentSection } from '@/features/note';
 
-export default function Document() {
+function Document({ user }) {
   const router = useRouter();
-  const { isOffline } = useAuth();
-  const { name } = router.query;
   const [isMounted, setIsMounted] = useState(false);
+
+  const { id, name } = router.query;
 
   useEffect(() => {
     setIsMounted(true);
@@ -20,30 +19,38 @@ export default function Document() {
 
   if (!isMounted || typeof window === 'undefined') return null;
 
-  if (isOffline) {
-    // todo handle offline mode here
-    // todo e.g. create provider with boolean value for offline mode
-    return <h1>You're in offline mode</h1>;
-  }
-
   return (
     <>
       <NextSeo title={name as string} noindex />
-      <AuthProtection loadingComponent={<LoadingPage />}>
-        <header className="fixed w-full p-5">
-          <button className="cursor-pointer" onClick={() => router.back()}>
-            <BsArrowBarLeft size={25} />
-          </button>
-        </header>
-        <div className="flex items-center justify-center w-screen">
-          <div className="flex flex-col order-1 p-16 w-full lg:w-2/3">
-            <h1 className="underline mb-12 text-5xl text-primary dark:text-zinc-400 ">{name}</h1>
-            <main className="h-screen">
-              <DocumentSection />
-            </main>
+      <AuthProvider preloadedUser={user}>
+        <AuthProtection loadingComponent={<LoadingPage />}>
+          <header className="fixed w-full p-5">
+            <button className="cursor-pointer" onClick={() => router.back()}>
+              <BsArrowBarLeft size={25} />
+            </button>
+          </header>
+          <div className="flex items-center justify-center w-screen">
+            <div className="flex flex-col order-1 p-16 w-full lg:w-2/3">
+              <h1 className="underline mb-12 text-5xl text-primary dark:text-zinc-400 ">{name}</h1>
+              <main className="h-screen">
+                <DocumentSection />
+              </main>
+            </div>
           </div>
-        </div>
-      </AuthProtection>
+        </AuthProtection>
+      </AuthProvider>
     </>
   );
 }
+
+export async function getServerSideProps(context) {
+  const { data: user } = await getUser(context.req.headers);
+
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+export default Document;
