dc294a865368f2f35749a6817f49e85766f02aec
GashonHussein <gashon96@gmail.com>
2023-04-10 17:01:20 -0700

    feat: implement quiz service

--- apps/api/src/domains/quiz/interfaces.ts
+++ apps/api/src/domains/quiz/interfaces.ts
@@ -14,11 +14,8 @@ interface IQuizController {
 interface IQuizService {
   findQuizById(id: string): Promise<QuizDocument | null>;
   createQuiz(createdBy: string, data: Partial<IQuiz>): Promise<QuizDocument>;
-  paginateQuizzes(condition: FilterQuery<QuizDocument>, query: PaginationParams): Promise<Partial<QuizListResponse>>;
   findQuizzesByIds(ids: string[]): Promise<Query<(QuizDocument & Required<{ _id: string; }>)[], QuizDocument & Required<{ _id: string; }>, {}, QuizDocument>>
-  updateQuizById(id: string, data: Partial<IQuiz>): Promise<Query<any, QuizDocument & Required<{ _id: string; }>, {}, QuizDocument>>
   deleteQuizById(id: string): Promise<Query<any, QuizDocument & Required<{ _id: string; }>, {}, QuizDocument>>;
-  paginateQuizzesByCollaboratorId(collaboratorId: string, query: PaginationParams): Promise<Partial<QuizListResponse>>
 }
 
 export { IQuiz, IQuizController, IQuizService }; 
