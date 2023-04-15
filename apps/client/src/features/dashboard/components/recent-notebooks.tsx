c5c8c0b0089f1badc16eb83f7658c4174debbb8e
GashonHussein <gashon96@gmail.com>
2023-04-14 19:13:27 -0700

    fix: no notes presentation

--- apps/client/src/features/dashboard/components/recent-notebooks.tsx
+++ apps/client/src/features/dashboard/components/recent-notebooks.tsx
@@ -9,8 +9,13 @@ export const RecentNotebooks: FC<{}> = ({}) => {
   const { data: notes, isFetched } = useRecentNotes(user.id);
   const { mutate: deleteNote } = useDeleteNote();
 
+  const noNotes = isFetched && notes.data.length === 0;
+
   return (
     <>
+     {!noNotes && <h3 className="pb-3 text-xs text-zinc-600 dark:text-zinc-400 lg:text-base">
+        Recently edited notebooks
+      </h3>}
       <div className="flex flex-col gap-3">
         {!isFetched && (
           <>
