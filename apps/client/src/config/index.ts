830ab83896af933160f95f053fa1e83144c697dd
GashonHussein <gashon96@gmail.com>
2023-04-18 16:07:49 -0700

    chore: dockerize build

--- apps/client/src/config/index.ts
+++ apps/client/src/config/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 export const JWT_SECRET = '123456' as string;
