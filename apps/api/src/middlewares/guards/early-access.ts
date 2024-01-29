a7997c906959b2c80259d478704f97265a7436a2
gashon <gashon96@gmail.com>
2024-01-29 12:51:52 -0800

    fix: nifty typing

--- apps/api/src/middlewares/guards/early-access.ts
+++ apps/api/src/middlewares/guards/early-access.ts
@@ -1,37 +1,48 @@
-import { RequestHandler, Request, Response, NextFunction } from "express";
-import status from "http-status";
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+import status from 'http-status';
 
-import Token from "@nifty/server-lib/models/token";
-import { IUser } from "@nifty/server-lib/models/user";
-import { USER_PERMISSIONS } from "@/../../../packages/common/constants";
+import Token from '@nifty/server-lib/models/token';
+import { IUser } from '@nifty/server-lib/models/user';
+import { USER_PERMISSIONS } from '@nifty/common/constants';
 import { ACCESS_TOKEN_NAME } from '@/constants';
 
-const WHITE_LIST = [
-  '/v1/users/subscribe',
-]
+const WHITE_LIST = ['/v1/users/subscribe'];
 
-const earlyAccessGuard: RequestHandler = async (req: Request,
+const earlyAccessGuard: RequestHandler = async (
+  req: Request,
   res: Response,
-  next: NextFunction) => {
-
+  next: NextFunction
+) => {
   for (const path of WHITE_LIST) {
     if (req.url === path) {
       return next();
     }
   }
 
-  const accessToken = await Token.findById(req.cookies[ACCESS_TOKEN_NAME]).populate<{ user: IUser }>(
-    'user'
-  );
+  const accessToken = await Token.findById(
+    req.cookies[ACCESS_TOKEN_NAME]
+  ).populate<{ user: IUser }>('user');
 
   // redirect to referrer if not early access
-  if (!accessToken?.user?.permissions?.includes(USER_PERMISSIONS.EARLY_ACCESS)) {
-    if (req.url?.includes("ajax") && !req.url?.includes("email"))
-      return res.status(status.UNAUTHORIZED).json({ error: { message: 'You do not have early access to this feature.', type: 'invalid_request_error' } });
-    return res.status(status.FORBIDDEN).json({ error: { message: 'You do not have early access to this feature.', type: 'invalid_request_error' } });
+  if (
+    !accessToken?.user?.permissions?.includes(USER_PERMISSIONS.EARLY_ACCESS)
+  ) {
+    if (req.url?.includes('ajax') && !req.url?.includes('email'))
+      return res.status(status.UNAUTHORIZED).json({
+        error: {
+          message: 'You do not have early access to this feature.',
+          type: 'invalid_request_error',
+        },
+      });
+    return res.status(status.FORBIDDEN).json({
+      error: {
+        message: 'You do not have early access to this feature.',
+        type: 'invalid_request_error',
+      },
+    });
   }
 
   next();
-}
+};
 
 export default earlyAccessGuard;
