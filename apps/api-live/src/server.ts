7a03f5ab02173d3523a3b56b7c3af3372d327436
gashon <gashon96@gmail.com>
2024-03-31 13:10:35 -0700

    feat: support socket authentication

--- apps/api-live/src/server.ts
+++ apps/api-live/src/server.ts
@@ -1,13 +1,60 @@
 import { Database } from '@hocuspocus/extension-database';
 import { Logger } from '@hocuspocus/extension-logger';
-import { Server } from '@hocuspocus/server';
+import { onAuthenticatePayload, Server } from '@hocuspocus/server';
+import { verifyToken } from '@nifty/api/lib/jwt';
+import { isPermitted, Permission } from '@nifty/api/util/handle-permissions';
+import { AccessTokenJwt } from '@nifty/common/types';
 import { mkdir, readFile, writeFile } from 'fs/promises';
 import * as Y from 'yjs';
 import { DBRepository } from './db';
+import { getAccessTokenString } from './socket';
 
 const dbRepository = new DBRepository();
 
 const server = Server.configure({
+  onAuthenticate: async (data: onAuthenticatePayload) => {
+    const tokenStr = getAccessTokenString(data.requestHeaders.cookie);
+    const token = verifyToken<AccessTokenJwt>(tokenStr);
+    if (!token) {
+      throw new Error('Unauthorized');
+    }
+
+    const documentId = Number(data.documentName);
+
+    const notePermissions = await dbRepository.getNotePermissions(documentId);
+
+    const hasPublicWritePermissions = isPermitted(
+      await dbRepository.getNotePermissions(documentId),
+      Permission.ReadWrite
+    );
+    const hasPublicPermissionsRead = isPermitted(
+      notePermissions,
+      Permission.Read
+    );
+    const hasPublicPermissions =
+      hasPublicPermissionsRead || hasPublicWritePermissions;
+    if (hasPublicPermissions) {
+      if (!hasPublicWritePermissions) {
+        data.connection.readOnly = true;
+      }
+      return { user: token.user };
+    }
+
+    const collaborator = await dbRepository.getCollaborator(
+      token.user.id,
+      documentId
+    );
+    if (!collaborator) throw new Error('Unauthorized');
+
+    if (!isPermitted(collaborator.permissions, Permission.ReadWrite)) {
+      data.connection.readOnly = true;
+    }
+
+    // update last viewed at asynchonously
+    dbRepository.updateCollaboratorLastViewedAt(collaborator.id);
+
+    return { user: token.user };
+  },
   extensions: [
     new Logger(),
     new Database({
@@ -26,8 +73,7 @@ const server = Server.configure({
         return doc;
       },
       store: async ({ documentName, state }) => {
-        await dbRepository.writeNoteToDisk(documentName, state);
-        await writeFile(`./data/${documentName}.bin`, state);
+        await dbRepository.writeNoteToDisk(Number(documentName), state);
       },
     }),
   ],
@@ -35,23 +81,15 @@ const server = Server.configure({
   onLoadDocument: async ({ documentName }) => {
     const doc = new Y.Doc();
 
-    console.log('doc created', documentName);
-
     const content = doc.getMap('content');
 
     return doc;
   },
 
-  onStoreDocument: async ({ documentName, document }) => {
-    await writeFile(
-      `./data/${documentName}.json`,
-      JSON.stringify(document.getMap('content').toJSON())
-    );
-  },
+  onStoreDocument: async ({ documentName, document }) => {},
 });
 
 const main = async () => {
-  await mkdir('./data', { recursive: true }).catch(() => null);
   await server.listen(8080);
   console.log('Server is running on port 8080');
 };
