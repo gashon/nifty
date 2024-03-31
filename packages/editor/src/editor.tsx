d5bffb1c2122681edbc78e0f9c0e58fddc688fc0
gashon <gashon96@gmail.com>
2024-03-31 16:18:13 -0700

    refactor: useSocket custom hook

--- packages/editor/src/editor.tsx
+++ packages/editor/src/editor.tsx
@@ -21,75 +21,17 @@ import * as Y from 'yjs';
 import type { Selectable, User } from '@nifty/common/types';
 
 import { MenuBar } from './components/menu-bar';
-
-const ydoc = new Y.Doc();
+import { useSocket } from './hooks';
 
 export const Editor: FC<{ user: Selectable<User>; documentId: string }> = ({
   user,
   documentId,
 }) => {
-  const [status, setStatus] = useState<WebSocketStatus>(
-    WebSocketStatus.Connecting
-  );
-  console.log('user', user);
-
   if (!documentId) {
     return <div>No documentId provided</div>;
   }
 
-  const socket = useMemo(() => {
-    return new TiptapCollabProviderWebsocket({
-      baseUrl: 'ws://localhost:8080',
-      connect: true,
-    });
-  }, []);
-
-  const websocketProvider = useMemo(() => {
-    return new TiptapCollabProvider({
-      name: documentId,
-      document: ydoc,
-      websocketProvider: socket,
-      onAuthenticationFailed: ({ reason }) => {
-        console.log('reason', reason);
-      },
-      onStatus: ({ status }) => {
-        setStatus(status);
-      },
-    });
-  }, [socket, documentId, ydoc]);
-
-  const editor = useEditor({
-    extensions: [
-      StarterKit.configure({
-        history: false,
-      }),
-      Highlight,
-      TaskList,
-      TaskItem,
-      CharacterCount.configure({
-        limit: 10000,
-      }),
-      Collaboration.configure({
-        document: ydoc,
-      }),
-      CollaborationCursor.configure({
-        provider: websocketProvider,
-      }),
-    ],
-    // update user
-    onUpdate: ({ editor }) => {
-      if (editor && user) {
-        editor
-          .chain()
-          .focus()
-          .updateUser({
-            name: user.email,
-            avatar: user.avatarUrl,
-          })
-          .run();
-      }
-    },
-  });
+  const { status, editor } = useSocket(documentId, user);
 
   return (
     <div className="editor">
@@ -97,9 +39,11 @@ export const Editor: FC<{ user: Selectable<User>; documentId: string }> = ({
       <EditorContent className="editor__content" editor={editor} />
       <div className="editor__footer">
         <div className={`editor__status editor__status--${status}`}>
-          {status === 'connected'
-            ? `${editor.storage.collaborationCursor.users.length} user${
-                editor.storage.collaborationCursor.users.length === 1 ? '' : 's'
+          {status === WebSocketStatus.Connected
+            ? `${editor?.storage.collaborationCursor.users.length} user${
+                editor?.storage.collaborationCursor.users.length === 1
+                  ? ''
+                  : 's'
               } online in ${documentId}`
             : 'offline'}
         </div>
---
+++ packages/editor/src/hooks/use-editor.ts
@@ -0,0 +1,85 @@
+import { useMemo, useState } from 'react';
+import * as Y from 'yjs';
+import {
+  HocuspocusProviderWebsocket,
+  HocuspocusProvider,
+  TiptapCollabProviderWebsocket,
+  TiptapCollabProvider,
+  WebSocketStatus,
+} from '@hocuspocus/provider';
+import { EditorContent, useEditor } from '@tiptap/react';
+import CharacterCount from '@tiptap/extension-character-count';
+import Collaboration from '@tiptap/extension-collaboration';
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor';
+import Highlight from '@tiptap/extension-highlight';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import StarterKit from '@tiptap/starter-kit';
+import { Selectable, User } from '@nifty/common/types';
+
+export const useSocket = (documentId: string, user: Selectable<User>) => {
+  const [status, setStatus] = useState<WebSocketStatus>(
+    WebSocketStatus.Connecting
+  );
+  const ydoc = useMemo(() => {
+    const ydoc = new Y.Doc();
+    return ydoc;
+  }, []);
+
+  const socket = useMemo(() => {
+    return new TiptapCollabProviderWebsocket({
+      baseUrl: 'ws://localhost:8080',
+      connect: true,
+    });
+  }, []);
+
+  const websocketProvider = useMemo(() => {
+    return new TiptapCollabProvider({
+      name: documentId,
+      document: ydoc,
+      websocketProvider: socket,
+      onAuthenticationFailed: ({ reason }) => {
+        console.log('reason', reason);
+      },
+      onStatus: ({ status }) => {
+        setStatus(status);
+      },
+    });
+  }, [socket, documentId, ydoc]);
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        history: false,
+      }),
+      Highlight,
+      TaskList,
+      TaskItem,
+      CharacterCount.configure({
+        limit: 10000,
+      }),
+      Collaboration.configure({
+        document: ydoc,
+      }),
+      CollaborationCursor.configure({
+        provider: websocketProvider,
+      }),
+    ],
+    // update user
+    onUpdate: ({ editor }) => {
+      if (editor && user) {
+        editor
+          .chain()
+          .focus()
+          .updateUser({
+            name: user.email,
+            avatar: user.avatarUrl,
+            // color:
+          })
+          .run();
+      }
+    },
+  });
+
+  return { editor, status, websocketProvider, ydoc };
+};
