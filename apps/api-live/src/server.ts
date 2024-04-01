807c2213b31e7543bf9eed2c6c492b02e12780cb
gashon <gashon96@gmail.com>
2024-04-01 02:29:09 -0700

    fix: pg docker port

--- apps/api-live/src/server.ts
+++ apps/api-live/src/server.ts
@@ -81,10 +81,7 @@ const server = Server.configure({
 });
 
 const main = async () => {
-  await server.listen({
-    port: 8080,
-    path: '/ws',
-  });
+  await server.listen(8080);
   console.log('Server is running on port 8080');
 };
 
